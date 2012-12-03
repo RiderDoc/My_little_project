041d1c54a904f0562414603893af654537d67965
RiderDoc <Emil.olesen@gmail.com>
2012-12-03 13:56:09 +0100

    Pushed variables py module

---
+++ src/variables.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 03/12/2012
+
+@author: Emil
+'''
+# exampels of names
+
+def This_is_a_name():
+    print("this is a string value")
+    print(1) #this is a int value
+    
